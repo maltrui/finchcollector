2437584e07f922df4480dbcb5b01c1ead15d3b6d
maltrui <mcaltrui@gmail.com>
2022-12-19 15:25:00 -0500

    Part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+class Finch: 
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Mike', 'Finch 1', 'Weird Finch', 3),
+  Finch('Bro', 'Finch 2', 'Cool Finch', 0),
+  Finch('Sam', 'Finch 3', 'Normal Finch', 4)
+]
+
+def home(request):
+    return HttpResponse('<h1>Home</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', { 'finches': finches })
