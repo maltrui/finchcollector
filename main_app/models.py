5cf661de5b7e4b0cfe65fc7f1554537bc97b45db
maltrui <mcaltrui@gmail.com>
2022-12-20 14:16:24 -0500

    Part 2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=200)
+    breed = models.CharField(max_length=200)
+    description = models.TextField(max_length=300)
+    age = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,7 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
-class Finch: 
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Mike', 'Finch 1', 'Weird Finch', 3),
-  Finch('Bro', 'Finch 2', 'Cool Finch', 0),
-  Finch('Sam', 'Finch 3', 'Normal Finch', 4)
-]
+from .models import Finch
 
 def home(request):
     return HttpResponse('<h1>Home</h1>')
@@ -22,4 +10,9 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html',{ 'finch': finch})
